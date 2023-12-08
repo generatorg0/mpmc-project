0b67efedee649655260f1da96fd33254f3326319
Azmi Nabeel <azmihero158@gmail.com>
2023-12-08 19:51:42 +0530

    Added script exec and upload image

--- public/script.js
+++ public/script.js
@@ -1,33 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   alert("hi");
-  const button = document.getElementById("runCommandBtn");
+  const button = document.getElementById("executeButton");
+  const uploadBtn = document.getElementById("uploadbtn");
   const outputContainer = document.getElementById("outputContainer");
 
-  document
-    .getElementById("executeButton")
-    .addEventListener("click", function () {
-      // Make an AJAX request to trigger the Python script execution
-      alert("python");
-      var xhr = new XMLHttpRequest();
-      xhr.open("POST", "/run_script", true);
-      xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-      xhr.send();
-
-      xhr.onload = function () {
-        if (xhr.status === 200) {
-          console.log(xhr.responseText);
-        } else {
-          console.error("Error:", xhr.statusText);
-        }
-      };
-    });
+  // button.addEventListener("click", () => {
+  //   alert("hi");
+  //   const command = prompt("Enter command:", "ls");
+  //   if (command !== null) {
+  //     runCommand(command);
+  //   }
+  // });
+  // custom command run
 
   button.addEventListener("click", () => {
     alert("hi");
-    const command = prompt("Enter command:", "ls");
+    const command = "python helloworld.py";
     if (command !== null) {
       runCommand(command);
     }
+  });
+
+  uploadBtn.addEventListener("click", () => {
+    alert("Upload Image");
   });
 
   function runCommand(command) {
@@ -42,3 +37,22 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   }
 });
+
+// document
+// .getElementById("executeButton")
+// .addEventListener("click", function () {
+//   // Make an AJAX request to trigger the Python script execution
+//   alert("python");
+//   var xhr = new XMLHttpRequest();
+//   xhr.open("POST", "/run_script", true);
+//   xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+//   xhr.send();
+
+//   xhr.onload = function () {
+//     if (xhr.status === 200) {
+//       console.log(xhr.responseText);
+//     } else {
+//       console.error("Error:", xhr.statusText);
+//     }
+//   };
+// });
